02e9599a29e491b0121aa289bdec8e50f72e8284
Tom Elliott <tom.m.elliott88@gmail.com>
2018-11-11 15:15:02 +0000

    Update JSON payload

--- src/data.js
+++ src/data.js
@@ -1,5 +1,36 @@
 const data = {
-  "something": "something else"
+  "holidays" : [
+    {
+      "name": "Tom Elliott",
+      "start_date": "21Nov18",
+      "end_date": "8Dec18",
+      "location" : "Brazil"
+    },
+    {
+      "name": "Nikki Christofi",
+      "start_date": "2Dec18",
+      "end_date": "2Jan19",
+      "location" : "Portugal"
+    },
+    {
+      "name": "Mark Jeoffries",
+      "start_date": "12Nov18",
+      "end_date": "16Nov18",
+      "location" : "York"
+    },
+    {
+      "name": "Amy Smith",
+      "start_date": "22Nov18",
+      "end_date": "30Nov18",
+      "location" : "Glasgow"
+    },
+    {
+      "name": "Aubrey Graham",
+      "start_date": "22Nov18",
+      "end_date": "30Nov19",
+      "location" : "Barbados"
+    }
+  ]
 }
 
 module.exports = data
